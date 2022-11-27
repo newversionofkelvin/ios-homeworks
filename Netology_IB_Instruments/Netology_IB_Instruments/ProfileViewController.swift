7014bdff0c918e6c7551307a46415c2b2a70f53e
newversionofkelvin <100484848+newversionofkelvin@users.noreply.github.com>
2022-11-27 22:52:05 +0300

    Домашнее задание UI #1

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Alexander Novak on 27.11.2022.
+//
+
+import UIKit
+
+final class ProfileViewController: UIView {
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
+        
+
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Alexander Novak on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
